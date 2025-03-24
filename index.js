3207a0a423c5b3380fa19925db67f2ab9a5135e5
Pranay Shinde <PranayShinde486@gmail.com>
2025-03-24 23:22:19 +0530

    Chat app Completed

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js")
+const methodOverride = require("method-override")
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({ extended: true }))
-
+app.use(methodOverride("_method"))
 
 main().then(() => {
     console.log("Connection Successful")
@@ -34,6 +35,14 @@ app.get("/chats/:id/edit", async (req, res) => {
     res.render("edit.ejs", { chat })
 })
 
+app.put("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let { msg: newMsg } = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(id, { msg: newMsg }, { runValidators: true })
+    console.log(updatedChat)
+    res.redirect("/chats")
+})
+
 
 
 app.get("/chats", async (req, res) => {
@@ -47,7 +56,8 @@ app.post("/chats", (req, res) => {
     let newChat = new Chat({
         from: from,
         to: to,
-        msg: msg
+        msg: msg,
+        created_at: new Date()
     });
 
     console.log(newChat)
@@ -59,6 +69,12 @@ app.post("/chats", (req, res) => {
 
     res.redirect("/chats")
 
+})
+
+app.delete("/chats/:id", async (req, res) => {
+    let {id} = req.params;
+    await Chat.findByIdAndDelete(id)
+    res.redirect("/chats")
 })
 
 
